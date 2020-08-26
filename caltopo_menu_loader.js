1072303fda4c90233e6462916c64eb3f002cc98d
Rob Donnelly <rfdonnelly@gmail.com>
2020-08-26 16:52:53 -0700

    fix: remove script from page after it has run

--- caltopo_menu_loader.js
+++ caltopo_menu_loader.js
@@ -1,9 +1,9 @@
 function onload() {
   var script = document.createElement('script');
   script.src = chrome.runtime.getURL('caltopo_menu.js');
-  // script.onload = function() {
-  //   this.remove();
-  // };
+  script.onload = function() {
+    this.remove();
+  };
   document.body.appendChild(script);
 }
 
